b56ee3ab5ab3619128e1bad993de2003e6eacfff
danba340 <danba340@gmail.com>
2020-01-02 21:59:35 +0100

    POC finish

---
+++ src/js/data.js
@@ -0,0 +1,66 @@
+export const data = [
+  {
+    start: "Star_Trek",
+    finish: "Gene_Roddenberry"
+  },
+  {
+    start: "Battle_of_Calais",
+    finish: "Figging"
+  },
+  {
+    start: "Star_Trek",
+    finish: "Uganda"
+  },
+  {
+    start: "Psoriasis",
+    finish: "Tormod"
+  },
+  {
+    start: "Gavid_DeGraw",
+    finish: "Gothenburg"
+  },
+  {
+    start: "Roman_art",
+    finish: "Burger_King"
+  },
+  {
+    start: "Pizza",
+    finish: "Nelson_Mandela"
+  },
+  {
+    start: "Sodium",
+    finish: "Salmon"
+  },
+  {
+    start: "Apple_inc",
+    finish: "Machete"
+  },
+  {
+    start: "Harry_Potter",
+    finish: "Carnitas"
+  },
+  {
+    start: "Star_Wars",
+    finish: "Drinking_game"
+  },
+  {
+    start: "Banana",
+    finish: "Julia_Roberts"
+  },
+  {
+    start: "Star",
+    finish: "Brunost"
+  },
+  {
+    start: "Cereal",
+    finish: "Lofoten"
+  },
+  {
+    start: "Beer",
+    finish: "Top_Gun"
+  },
+  {
+    start: "First_Mexican_Empire",
+    finish: "Religious_male_circumcision"
+  }
+];
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,139 @@
-import 'bootstrap';
+import "bootstrap";
 
-import '../scss/index.scss';
+import "../scss/index.scss";
 
-$('#alert').click(() => {
-  alert('jQuery works!');
+import { data } from "./data.js";
+
+const wiki = $("#wiki");
+
+const URL = "https://en.wikipedia.org/w/api.php";
+
+const HISTORY = [];
+
+var PARAMS = {
+  action: "parse",
+  format: "json"
+};
+
+function createUrl(page) {
+  let url = URL + "?origin=*";
+  let params = {
+    ...PARAMS,
+    page
+  };
+  Object.keys(params).forEach(function(key) {
+    url += "&" + key + "=" + params[key];
+  });
+  return url;
+}
+
+function getWikiPage(page) {
+  setLoading();
+  fetch(createUrl(page))
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      wiki.html(response.parse.text["*"]);
+      bindWikiLinks();
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function bindWikiLinks() {
+  let count = 0;
+  $("#wiki a").each((_, link) => {
+    if (link && link.href && link.href.includes("/wiki/")) {
+      const href = link.href.split("/wiki/")[1];
+      $(link).on("click", function(e) {
+        e.preventDefault();
+        onWikiLinkClick(href);
+      });
+    } else {
+      $(link).on("click", function(e) {
+        e.preventDefault();
+      });
+    }
+  });
+}
+
+function onWikiLinkClick(link) {
+  const pieces = link.split("/");
+  const page = pieces[pieces.length - 1];
+  HISTORY.push(page);
+  localStorage.setItem("history", HISTORY);
+  if (checkWinningCondition(page)) {
+    localStorage.setItem("won", "1");
+    showWinningPage();
+  } else {
+    getWikiPage(page);
+  }
+}
+
+function showWinningPage() {
+  window.location.href = "history.html";
+}
+
+function checkWinningCondition(page) {
+  const winner = page === localStorage.getItem("finish");
+  if (winner) {
+    console.log("winner");
+  } else {
+    console.log("keep trying");
+  }
+  return winner;
+}
+
+function setLoading() {
+  wiki.html('<div class="center" >Loading...</div>');
+}
+
+function onStart() {
+  localStorage.setItem("history", "");
+  const selectedValue = $("#numberPicker")[0].value;
+  const selectedData = data[selectedValue - 1];
+  localStorage.setItem("finish", selectedData.finish);
+  getWikiPage(createUrl(selectedData.start));
+  console.log(data[selectedValue]);
+}
+
+function onStop() {
+  localStorage.setItem("history", "");
+  window.location.reload();
+}
+
+function cleanPageTitle(title) {
+  return title.split("_").join(" ");
+}
+
+$("#start").click(event => {
+  event.preventDefault();
+  onStart();
 });
 
-// Your jQuery code
+$("#stop").click(event => {
+  onStop();
+});
+
+$("#numberPicker").change(function() {
+  const val = $(this).val() - 1;
+  $("#from").html(cleanPageTitle(data[val].start));
+  $("#to").html(cleanPageTitle(data[val].finish));
+});
+$("#numberPicker").trigger("change");
+
+if (window.location.href.includes("history")) {
+  const wonDiv = $("#won");
+  if (localStorage.getItem("won") === "1") {
+    wonDiv.show();
+    localStorage.setItem("won", "0");
+  }
+  const historyDiv = $("#history");
+  const historyItems = localStorage.getItem("history").split(",");
+  historyItems.forEach(item => {
+    const currentContent = historyDiv.html();
+    historyDiv.html(`${currentContent}<li>${cleanPageTitle(item)}</li>`);
+  });
+}
